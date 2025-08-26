9a98da522edced1fd25f987c535cb3eaaa8432ab
Ganji Sai Kumar <ganjisaikumar23@gmail.com>
2025-08-26 10:32:20 +0530

    Error-Handling in user credential

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -3,24 +3,36 @@
 import React, { useState } from "react";
 import { loginAction } from "../serverActions/loginAction";
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 const page = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
+  const [error, setError] = useState("");
   const router = useRouter();
-  const registerHandler = (e) => {
+
+  const registerHandler = async (e) => {
     e.preventDefault();
 
     const userData = { email, password };
     console.log("userData", userData);
-    loginAction(userData);
-    router.push("/");
+    try {
+      const responce = await loginAction(userData);
+      if (responce.message) {
+        router.push("/");
+      } else {
+        setError("Invalid email or password");
+      }
+    } catch (error) {
+      setError("An error occurred during login");
+      console.log("error", error);
+    }
   };
 
   return (
     <div className="formContainer">
       <h2>Login form</h2>
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <form className="formSection" onSubmit={registerHandler}>
         <h3>Email</h3>
         <input
@@ -32,7 +44,7 @@ const page = () => {
         />
         <h3>Password</h3>
         <input
-          type="text"
+          type="password"
           name="password"
           onChange={(e) => {
             setPassword(e.target.value);
@@ -40,6 +52,9 @@ const page = () => {
         />
         <br /> <br />
         <button>Login</button>
+        <Link href="/register" style={{ marginLeft: "10px" }}>
+          New user? Register
+        </Link>
       </form>
     </div>
   );
